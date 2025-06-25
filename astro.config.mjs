10eeeda7d6fbcc9ad2616aeccdb06abca80f13b7
hayatonanba <hayatonanba@gmail.com>
2025-06-26 00:53:30 +0900

    feat: seo-planing

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
 	output: 'static',
-	site: 'https://example.com',
+	site: 'https://nababa-diary.hayatonanba.workers.dev',
 	integrations: [mdx(), sitemap()],
 
 	vite: {
