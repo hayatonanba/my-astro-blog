c6fd98e3eef9342c72e09950462dd12e4ca39ffc
hayatonanba <hayatonanba@gmail.com>
2025-06-25 10:17:45 +0900

    deploy-demo

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Nababa Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Nababa Diary';
+export const SITE_DESCRIPTION = '難波隼人の日記用の個人開発ブログです。';
